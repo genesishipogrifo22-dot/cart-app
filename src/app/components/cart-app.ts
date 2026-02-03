85f40a6fbabed056155bd93b915c51da9a4d5d20
Genesis <grodriguez@bancomercantil.com>
2026-02-03 08:17:45 -0400

    se crearon componentes nuevos

--- src/app/components/cart-app.ts
+++ src/app/components/cart-app.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'cart-app',
+  standalone: true,
   imports: [],
   templateUrl: './cart-app.html'
 })
-export class CartApp {
+export class CartApp implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private service: ProductService){}
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
 }
